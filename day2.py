738c26ef77d3c9cffb2ff18745bf626ea3ffdfe9
qiudandan <2324009918@qq.com>
2020-02-21 23:30:12 +0800

    加了装饰器的内容

--- day2.py
+++ day2.py
@@ -10,3 +10,16 @@ class Dog(object):
         # 定义类的方法
     def shout(self):
         print('I\'m %s, type: %s' % (self.name, self.dtype))
+
+        
+     #   class Book(object):
+     #     def __init__(self,name,sale):
+     #         self.__name = name
+     #         self.__sale = sale
+     #     @property
+     #     def name(self):
+     #         return self.__name
+     #     @name.setter
+     #     def name(self,new_name):
+     #         self.__name = new_name
+     # 注意这种装饰器写法：name.setter，name 已经被包装为 property 实例，调用实例上的 setter 函数再包装 name 后就会可写。
